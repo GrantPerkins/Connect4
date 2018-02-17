f88c3defdac712b2994c73e70d2f6ae771b838c8
GrantPerkins <gcperk20@gmail.com>
2018-02-16 21:25:26 -0500

    Auto checks to see if anyone's won

--- Application.py
+++ Application.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.font as font
+import sys
 from Button import Button
 root = tk.Tk()
 
@@ -19,7 +21,7 @@ class Game:
         button_windows = []
 
         for i in range(7):
-            buttons.append(Button(i))
+            buttons.append(Button(root, game, i))
             button_windows.append(self.canvas.create_window(i * 100, 0, anchor=tk.NW, window=buttons[-1]))
         for y in range(100, 7 * 100, 100):
             for x in range(0, 7 * 100, 100):
@@ -31,11 +33,15 @@ class Game:
         while True:
             if index<6 and self.board[index][column] == None:
                 index += 1
-            else:
+            elif index != 0:
                 index -= 1
                 self.board[index][column] = team
                 self.draw_chip(column*100, (index+1)*100, team)
                 self.turn = self.turn ^ 1
+                if self.process():
+                    sys.exit(99)
+                break
+            else:
                 break
 
     def draw_chip(self, x, y, team):
@@ -44,6 +50,67 @@ class Game:
         else:
             color = "yellow"
         self.canvas.create_oval(x,y,x+100,y+100,fill=color)
+
+    def process(self):
+        print("-----------------===================--------------------------")
+        for y in range(6):
+            for x in range(7):
+                for direction in range(4):
+                    if self.board[y][x] != None and self.check_four(0, self.board[y][x], x, y, direction):
+                        print(direction)
+                        return True
+        return False
+
+    def check_four(self, n, team, x, y, direction):
+        """
+        n:
+            Number of chips of same color in a row so far; used for recursion
+        direction:
+            0: horizontal to left
+            1: diagonal left
+            2: down
+            3: diagonal right
+        """
+        if self.board[y][x] == team:
+            if direction == 0:
+                if x == 6:
+                    return False
+                elif n==3:
+                    return True
+                else:
+                    print("horizontal", n, team, "x:", x, "y:", y)
+                    return self.check_four(n+1, team, x + 1, y, direction)
+            elif direction == 1:
+                if x == 6 or y == 5:
+                    if n==3 and y==5:
+                        return True
+                    return False
+                elif n==3:
+                    return True
+                else:
+                    #print("diagonal ", n, team, "x:", x, "y:", y, direction)
+                    return self.check_four(n + 1, team, x + 1, y + 1, direction)
+            elif direction == 2:
+                if y == 5:
+                    return False
+                elif n==3:
+                    return True
+                else:
+                    #print("vertical ", n, team, "x:", x, "y:", y, direction)
+                    return self.check_four(n + 1, team, x, y + 1, direction)
+            else:
+                if x == 0 or y == 5:
+                    if n == 3 and y == 5:
+                        return True
+                    return False
+                elif n == 3:
+                    return True
+                else:
+                    #print("diagonal ", n, team, "x:", x, "y:", y, direction)
+                    return self.check_four(n + 1, team, x - 1, y + 1, direction)
+        return False
+
+
 if __name__ == "__main__":
     game = Game()
     game.draw_grid()
--- Button.py
+++ Button.py
@@ -2,9 +2,7 @@ import tkinter as tk
 import tkinter.font as font
 
 class Button(tk.Button):
-    def __init__(self, root, n):
+    def __init__(self, root, game, n):
         super().__init__(root, text=str(n), command= lambda: game.add(n,game.turn), width=3, height=1,
             font=font.Font(family='Helvetica', size=36, weight='bold'))
         self.n = n
-    def callback(self):
-        print("click", self.n)
