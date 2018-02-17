979471741a264743c04da22ad502c71de6bc5389
Grant Perkins <24625614+GrantPerkins@users.noreply.github.com>
2018-02-17 11:03:49 -0500

    Merge conflict

--- Button.py
+++ Button.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.font as font
+
+print("breaking code")
 class Button(tk.Button):
     def __init__(self, root, game, n):
         super().__init__(root, text=str(n), command= lambda: game.add(n,game.turn), width=3, height=1,
